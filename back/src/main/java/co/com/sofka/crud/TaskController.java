7972ed17cf100f3edb9c3053dd0c8cfe9b9686ee
daviduribe0007 <daviduribe0007@gmail.com>
2021-02-26 22:25:58 -0500

    Se crea la clase TaskController

---
+++ back/src/main/java/co/com/sofka/crud/TaskController.java
@@ -0,0 +1,42 @@
+package co.com.sofka.crud;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:3000")
+public class TaskController {
+
+
+    @Autowired
+    private TaskDTA service;
+
+    @GetMapping(value = "task/all")
+    public Iterable<TaskDTO> list() {
+        return service.list();
+    }
+
+    @PostMapping(value = "task/post")
+    public TaskDTO save(@RequestBody TaskDTO taskDTO) {
+        return service.save(taskDTO);
+    }
+
+    @PutMapping(value = "task/put")
+    public TaskDTO update(@RequestBody TaskDTO tasDTO) {
+        if (tasDTO.getId() != null) {
+            return service.save(tasDTO);
+        }
+        throw new RuntimeException("No existe el id para actualziar");
+    }
+
+    @DeleteMapping(value = "task/{id}/delete")
+    public void delete(@PathVariable("id") Long id) {
+        service.delete(id);
+    }
+
+    @GetMapping(value = "task/{id}/get")
+    public TaskDTO get(@PathVariable("id") Long id) {
+        return service.get(id);
+    }
+
+}
