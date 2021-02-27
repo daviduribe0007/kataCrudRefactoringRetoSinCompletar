47f819fc4353617e5c2073b6a59e0e34ad409207
daviduribe0007 <daviduribe0007@gmail.com>
2021-02-26 22:29:10 -0500

    Se organiza el codigo de todoController y TaskController, tambien se dejan bien escritos los import de los dos

--- back/src/main/java/co/com/sofka/crud/TodoController.java
+++ back/src/main/java/co/com/sofka/crud/TodoController.java
@@ -1,7 +1,14 @@
 package co.com.sofka.crud;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:3000")
@@ -11,30 +18,30 @@ public class TodoController {
     private TodoDTA service;
 
     @GetMapping(value = "api/todos")
-    public Iterable<TodoDTO> list(){
+    public Iterable<TodoDTO> list() {
         return service.list();
     }
-    
+
     @PostMapping(value = "api/todo")
-    public TodoDTO save(@RequestBody TodoDTO todoDTO){
+    public TodoDTO save(@RequestBody TodoDTO todoDTO) {
         return service.save(todoDTO);
     }
 
     @PutMapping(value = "api/todo")
-    public TodoDTO update(@RequestBody TodoDTO todoDTO){
-        if(todoDTO.getId() != null){
+    public TodoDTO update(@RequestBody TodoDTO todoDTO) {
+        if (todoDTO.getId() != null) {
             return service.save(todoDTO);
         }
         throw new RuntimeException("No existe el id para actualziar");
     }
 
     @DeleteMapping(value = "api/{id}/todo")
-    public void delete(@PathVariable("id")Long id){
+    public void delete(@PathVariable("id") Long id) {
         service.delete(id);
     }
 
     @GetMapping(value = "api/{id}/todo")
-    public TodoDTO get(@PathVariable("id") Long id){
+    public TodoDTO get(@PathVariable("id") Long id) {
         return service.get(id);
     }
 
